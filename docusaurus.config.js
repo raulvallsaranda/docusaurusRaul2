1fe6743bc3858604678f5beb9a303aaf97c9c187
Raül Valls Aranda <r.vallsaranda@edu.gva.es>
2025-11-06 11:54:54 +0100

    commit desplegament github inicial

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://raulvallsaranda.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurusRaul2',
+  baseUrl: '/docusaurusRaul2/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
